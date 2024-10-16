a78a75e6b926b0f963e86f699ef2b34888d13399
LilKirill00 <pc.browsers.1@gmail.com>
2024-10-16 14:44:32 +0500

    style: убраны лишние коментарии

--- bot/bot.go
+++ bot/bot.go
@@ -101,10 +101,7 @@ func fillTemplateWithInfo(c *gin.Context, msg *messages.Message, text string) (r
 func getFileNames(root string) map[string]bool {
 	files := make(map[string]bool)
 
-	// строим абсолютный путь к переданной директории
 	root, _ = filepath.Abs(root)
-
-	// обходим все файлы и папки
 	filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
 		// проверяем, что текущий элемент не является директорией
 		if !info.IsDir() {
@@ -115,16 +112,13 @@ func getFileNames(root string) map[string]bool {
 	return files
 }
 
-// проверить является ли файл изображением
 func IsImage(file string) bool {
 	reImg, _ := regexp.Compile(`(?i)\.(png|jpg|jpeg|bmp)$`)
 
 	return reImg.MatchString(file)
 }
 
-// получить информацию о файле
 func getFileInfo(filename, filesDir string) (isImage bool, filePath string, err error) {
-	// строим абсолютный путь к файлу
 	fullName, _ := filepath.Abs(filepath.Join(filesDir, filename))
 
 	// проверяем есть ли файл в указанном месте
